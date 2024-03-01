f3197b417304a57dc74d8be5827b45483fc2507b
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-03-02 01:59:34 +0530

    Empty/New module for database hosting
    Signed-off-by: Vaisakh P S <vaisakh.sudheesh@gmail.com>

---
+++ Phase-2/Booking-Database/src/main/java/com/iisc/csa/pods/projects/bookingdatabase/BookingDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.iisc.csa.pods.projects.bookingdatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookingDatabaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BookingDatabaseApplication.class, args);
+    }
+
+}
---
+++ Phase-2/Booking-Database/src/test/java/com/iisc/csa/pods/projects/bookingdatabase/BookingDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.iisc.csa.pods.projects.bookingdatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookingDatabaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
