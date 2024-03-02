a66513bcf39259b00354de37ea0eb03282d3b49a
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-03-03 03:57:54 +0530

    Modified server creation code
    Signed-off-by: Vaisakh P S <vaisakh.sudheesh@gmail.com>

--- Phase-2/Booking-Database/src/main/java/com/iisc/csa/pods/projects/bookingdatabase/BookingDatabaseApplication.java
+++ Phase-2/Booking-Database/src/main/java/com/iisc/csa/pods/projects/bookingdatabase/BookingDatabaseApplication.java
@@ -4,6 +4,8 @@ package com.iisc.csa.pods.projects.bookingdatabase;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.context.annotation.Bean;
 import org.h2.tools.Server;
 
@@ -11,14 +13,33 @@ import java.sql.SQLException;
 
 
 @SpringBootApplication
-public class BookingDatabaseApplication {
+public class BookingDatabaseApplication extends SpringBootServletInitializer {
 
     public static void main(String[] args) {
+        startH2Server();
         SpringApplication.run(BookingDatabaseApplication.class, args);
     }
 
-    @Bean(initMethod = "start", destroyMethod = "stop")
-    public Server inMemoryH2DatabaseServer() throws SQLException {
-        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8084");
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        startH2Server();
+        return application.sources(BookingDatabaseApplication.class);
     }
+    private static void startH2Server() {
+        try {
+            Server h2Server = Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8084").start();
+            if (h2Server.isRunning(true)) {
+                System.out.println("H2 server was started and is running.");
+            } else {
+                throw new RuntimeException("Could not start H2 server.");
+            }
+        } catch (SQLException e) {
+            throw new RuntimeException("Failed to start H2 server: ", e);
+        }
+    }
+
+//    @Bean(initMethod = "start", destroyMethod = "stop")
+//    public Server inMemoryH2DatabaseServer() throws SQLException {
+//        return Server.createTcpServer("-tcp", "-tcpAllowOthers", "-tcpPort", "8084");
+//    }
 }
