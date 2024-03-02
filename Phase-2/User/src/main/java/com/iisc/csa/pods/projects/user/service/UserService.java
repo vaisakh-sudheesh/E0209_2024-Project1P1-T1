3b394a867f2a26efc65840f9e5b1680257d17c8c
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-03-02 23:48:44 +0530

    Removed unused imports from UserService.java
    Signed-off-by: Vaisakh P S <vaisakh.sudheesh@gmail.com>

--- Phase-2/User/src/main/java/com/iisc/csa/pods/projects/user/service/UserService.java
+++ Phase-2/User/src/main/java/com/iisc/csa/pods/projects/user/service/UserService.java
@@ -5,12 +5,8 @@ import com.iisc.csa.pods.projects.user.model.UserTable;
 import com.iisc.csa.pods.projects.user.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
