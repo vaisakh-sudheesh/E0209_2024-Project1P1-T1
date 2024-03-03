53d6d3bd3ad6b1e071c260ab8be414e1bbe9e603
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-03-03 12:54:46 +0530

    Take care of DB not being up while Booking starts
    There are chance of Booking-Database not being ready while Booking Application starts. Hence adding a Bean to take care of that dependency.
    
    It makes use of "DatabaseStartupValidator" to wait until DB is ready before proceeding.

--- Phase-2/Booking/src/main/java/com/iisc/csa/pods/projects/booking/BookingApplication.java
+++ Phase-2/Booking/src/main/java/com/iisc/csa/pods/projects/booking/BookingApplication.java
@@ -1,8 +1,16 @@
 package com.iisc.csa.pods.projects.booking;
 
+import jakarta.persistence.EntityManagerFactory;
+import org.springframework.beans.factory.config.BeanFactoryPostProcessor;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.jdbc.DatabaseDriver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jdbc.support.DatabaseStartupValidator;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.sql.DataSource;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 @EnableTransactionManagement
@@ -12,4 +20,19 @@ public class BookingApplication {
 		SpringApplication.run(BookingApplication.class, args);
 	}
 
+	@Bean
+	public DatabaseStartupValidator databaseStartupValidator (DataSource dataSource){
+		var dsv = new DatabaseStartupValidator();
+		dsv.setDataSource(dataSource);
+		dsv.setValidationQuery(DatabaseDriver.H2.getValidationQuery());
+		return dsv;
+	}
+
+	@Bean
+	public static BeanFactoryPostProcessor dependsOnPostProcessor() {
+		return bf -> {
+			String[] jpa = bf.getBeanNamesForType(EntityManagerFactory.class);
+			Stream.of(jpa).map(bf::getBeanDefinition).forEach(it -> it.setDependsOn("databaseStartupValidator"));
+		};
+	}
 }
