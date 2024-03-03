74a7847fa1832deb4c3716e3f468bfb64b84dfb7
Vaisakh P S <vaisakh.sudheesh@gmail.com>
2024-03-03 15:20:37 +0530

    Docker build script update
    Signed-off-by: Vaisakh P S <vaisakh.sudheesh@gmail.com>

--- Phase-2/Booking/build-n-run-docker.sh
+++ Phase-2/Booking/build-n-run-docker.sh
@@ -1,3 +1,3 @@
-docker build -t booking-service .
-docker run -p 8081:8080  --rm --name booking --add-host=host.docker.internal:host-gateway booking-service
+docker build -t vaisakhp/booking-service .
+docker run -p 8081:8080  --rm --name vaisakhp/booking --add-host=host.docker.internal:host-gateway vaisakhp/booking-service
 
--- Phase-2/User/build-n-run-docker.sh
+++ Phase-2/User/build-n-run-docker.sh
@@ -1,4 +1,4 @@
-docker build -t user-service .
-docker run -p 8080:8080 --rm --name user --add-host=host.docker.internal:host-gateway user-service
+docker build -t vaisakhp/user-service .
+docker run -p 8080:8080 --rm --name vaisakhp/user --add-host=host.docker.internal:host-gateway vaisakhp/user-service
 
 
